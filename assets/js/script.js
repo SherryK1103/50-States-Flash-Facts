c3b13fe6878259fe5e78c330ebf9baaa522d2bdd
Basilia Vega <basilia.vega5@gmail.com>
2023-07-13 19:29:37 -0500

    added psuedocode

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// View states on page load
+// Clickable cards
+// Modal opening upon click
+//  Modal bigger than text box 
+// Modal to populate current state facts
+// Capital 
+// Population
+// state flag
+// Weather
+// Year added to the U.S
+// Modal to minimize
+// Chosen card to remain highlighted
+// 
