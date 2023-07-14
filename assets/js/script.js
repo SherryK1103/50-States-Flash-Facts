1335c19068b7b7cb81929b2c82a73ca3a083ab03
YoonJae Kim <yoonjaekim3@gmail.com>
2023-07-13 20:26:02 -0500

    Added additional pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
+// HTML should have welcome nav bar
 // View states on page load
+//  > We need to pull our states from somewhere
+//  > We need to be able to create all of the cards using a function
+//  > The function must insert the state card into the html
 // Clickable cards
 // Modal opening upon click
-//  Modal bigger than text box 
+// Modal bigger than text box 
 // Modal to populate current state facts
 // Capital 
 // Population
@@ -10,4 +14,3 @@
 // Year added to the U.S
 // Modal to minimize
 // Chosen card to remain highlighted
-// 
