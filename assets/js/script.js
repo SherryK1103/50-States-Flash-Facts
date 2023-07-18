65bf8621f388828343933e1ed808aec7bd1dd348
YoonJae Kim <yoonjaekim3@gmail.com>
2023-07-17 20:48:48 -0500

    added all event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,13 +30,10 @@
 
 // Imported JSON array from https://gist.github.com/mshafrir/2646763
 const ALL_STATES = [{"name":"Alabama","abbreviation":"AL"},{"name":"Alaska","abbreviation":"AK"},{"name":"Arizona","abbreviation":"AZ"},{"name":"Arkansas","abbreviation":"AR"},{"name":"California","abbreviation":"CA"},{"name":"Colorado","abbreviation":"CO"},{"name":"Connecticut","abbreviation":"CT"},{"name":"Delaware","abbreviation":"DE"},{"name":"Florida","abbreviation":"FL"},{"name":"Georgia","abbreviation":"GA"},{"name":"Hawaii","abbreviation":"HI"},{"name":"Idaho","abbreviation":"ID"},{"name":"Illinois","abbreviation":"IL"},{"name":"Indiana","abbreviation":"IN"},{"name":"Iowa","abbreviation":"IA"},{"name":"Kansas","abbreviation":"KS"},{"name":"Kentucky","abbreviation":"KY"},{"name":"Louisiana","abbreviation":"LA"},{"name":"Maine","abbreviation":"ME"},{"name":"Maryland","abbreviation":"MD"},{"name":"Massachusetts","abbreviation":"MA"},{"name":"Michigan","abbreviation":"MI"},{"name":"Minnesota","abbreviation":"MN"},{"name":"Mississippi","abbreviation":"MS"},{"name":"Missouri","abbreviation":"MO"},{"name":"Montana","abbreviation":"MT"},{"name":"Nebraska","abbreviation":"NE"},{"name":"Nevada","abbreviation":"NV"},{"name":"New Hampshire","abbreviation":"NH"},{"name":"New Jersey","abbreviation":"NJ"},{"name":"New Mexico","abbreviation":"NM"},{"name":"New York","abbreviation":"NY"},{"name":"North Carolina","abbreviation":"NC"},{"name":"North Dakota","abbreviation":"ND"},{"name":"Ohio","abbreviation":"OH"},{"name":"Oklahoma","abbreviation":"OK"},{"name":"Oregon","abbreviation":"OR"},{"name":"Pennsylvania","abbreviation":"PA"},{"name":"Rhode Island","abbreviation":"RI"},{"name":"South Carolina","abbreviation":"SC"},{"name":"South Dakota","abbreviation":"SD"},{"name":"Tennessee","abbreviation":"TN"},{"name":"Texas","abbreviation":"TX"},{"name":"Utah","abbreviation":"UT"},{"name":"Vermont","abbreviation":"VT"},{"name":"Virginia","abbreviation":"VA"},{"name":"Washington","abbreviation":"WA"},{"name":"West Virginia","abbreviation":"WV"},{"name":"Wisconsin","abbreviation":"WI"},{"name":"Wyoming","abbreviation":"WY"}];
-let testState = "illinois";
-const containerEl = document.getElementById('container');
 
-
-function createCard (state) {
+function createCard (state, uniqueId) {
    return `
-   <div class="card has-background-info fifth-width">
+   <div id=${uniqueId} class="card has-background-info fifth-width">
         <div class="card-content">
             <div class="media">
                 <div class="media-content">
@@ -49,20 +46,23 @@ function createCard (state) {
     `
 }
 
-// document.getElementById("demo").id = "newid";
+const containerEl = document.getElementById('container');
 
 for (let i = 0; i < ALL_STATES.length; i++) {
-    let tenCounter = 0;
-    let stateCard = createCard(ALL_STATES[i])
-    console.log(stateCard);
-    containerEl.innerHTML += stateCard;
-    tenCounter++;
-    // containerEl.addEventListener('click', function() {
-    //   var cardEl = document.getElementById('card-1');
-    //   console.log(cardEl);
-    //   cardEl.classList.remove('has-background-info');
-    //   cardEl.classList.add('has-background-danger');
-    // });
+    let stateId = "card-" + i;
+    let stateCard = createCard(ALL_STATES[i], stateId)
 
+    containerEl.innerHTML += stateCard;
 }
 
+function cardClickHandler(event) {
+  const cardEl = event.currentTarget;
+  cardEl.classList.remove('has-background-info');
+  cardEl.classList.add('has-background-danger');
+}
+
+const cardElements = document.querySelectorAll('.card');
+
+cardElements.forEach(card => {
+  card.addEventListener('click', cardClickHandler);
+});
